b21725544745850cabab460d4ed7061ab341a451
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-06-28 20:02:16 -0300

    insert debug mode in twig

--- app/Provider/ViewServiceProvider.php
+++ app/Provider/ViewServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace App\Provider;
 
-
+use App\Config\Config;
 use League\Container\ServiceProvider\AbstractServiceProvider;
 use League\Container\ServiceProvider\BootableServiceProviderInterface;
 use App\Views\View;
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
 class ViewServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
@@ -17,14 +18,17 @@ class ViewServiceProvider extends AbstractServiceProvider implements BootableSer
         $this->getContainer()->add(View::class, function () {
             $loader = new FilesystemLoader(__DIR__ . '/../../resources/views');
 
+            $debug = $this->getContainer()->get(Config::class)->get('app.debug');
+
             $twig = new Environment(
                 $loader,
                 [
                     'cache' => false,
-                    'debug' => true
+                    'debug' => $debug,
                 ]
             );
 
+            $twig->addExtension(new DebugExtension());
 
             return new View($twig);
         });
