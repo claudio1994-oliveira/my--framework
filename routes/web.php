34a7f44a2547de8ae85f3bed205498e9f87c5145
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-07-15 18:24:42 -0300

    creating the registration flow

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,15 @@ use Psr\Container\ContainerInterface;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\RegisterController;
 
 return static function (Router $router, ContainerInterface $container) {
 
     $router->get('/', HomeController::class);
     $router->get('/dashboard', DashboardController::class);
+
+    $router->get('/register', [RegisterController::class, "index"]);
+    $router->post('/register', [RegisterController::class, "store"]);
 
     $router->get('/users/{user}', UserController::class);
 };
