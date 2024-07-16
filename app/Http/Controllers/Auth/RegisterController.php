644ab99c1c7cc278f8b9f0fb67bda9888e5e93b0
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-07-16 11:45:43 -0300

    registering and authenticating a user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Auth;
 
 
 use App\Views\View;
+use Cartalyst\Sentinel\Sentinel;
 use Laminas\Diactoros\Response;
-
+use Laminas\Diactoros\Response\RedirectResponse;
 use Psr\Http\Message\ServerRequestInterface;
 
 class RegisterController
 {
-    public function __construct(protected View $view)
+    public function __construct(protected View $view, protected Sentinel $auth)
     {
     }
 
@@ -31,5 +32,11 @@ class RegisterController
 
     public function store(ServerRequestInterface $request)
     {
+        if ($user = $this->auth->registerAndActivate($request->getParsedBody())) {
+            $this->auth->login($user);
+        }
+
+
+        return new RedirectResponse('/dashboard');
     }
 }
